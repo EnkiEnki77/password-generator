b619f5b876a9f878ba952f8957081a3d3bdb2753
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2022-10-08 09:50:44 -0700

    configured slider state logic

--- components/PasswordParameters.tsx
+++ components/PasswordParameters.tsx
@@ -10,12 +10,12 @@ const PasswordParameters = (props: Props) => {
     const checkArray: [string, string, string, string] = 
     ['Include Uppercase Letters', 'Include Lowercase Letters', 'Include Numbers', 'Include Numbers']
   return (
-    <div className="w-full h-[423px] md:h-[528px] max-w-[540px] bg-darkGrey md:p-8 px-4 pb-4 pt-5 flex flex-col">
+    <form className="w-full h-[423px] md:h-[528px] max-w-[540px] bg-darkGrey md:p-8 px-4 pb-4 pt-5 flex flex-col">
         <Slider/>
         {checkArray.map(item => <Checkbox label={item}/>)}
         <PasswordStrength/>
         <Button/>
-    </div>
+    </form>
     
   )
 }
